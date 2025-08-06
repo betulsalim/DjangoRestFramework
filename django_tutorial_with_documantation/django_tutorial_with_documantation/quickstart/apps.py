c1921f1d8050825e7ab905bca8b7c3f891a40e1b
Betül Salim <betulsalim6@gmail.com>
2025-08-06 20:57:21 +0300

    Ornek Django Rest Framework

--- django_tutorial_with_documantation/django_tutorial_with_documantation/quickstart/apps.py
+++ django_tutorial_with_documantation/django_tutorial_with_documantation/quickstart/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class QuickstartConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'quickstart'
+    name = 'django_tutorial_with_documantation.quickstart'
